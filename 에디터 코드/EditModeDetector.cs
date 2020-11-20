a935c6bc3e2ca9a41ebc1662645304a8e339f4bf
이세종 <sejong@433.co.kr>
2020-11-20 11:26:05 +0900

    에디터에서 실행중에 컴파일을 멈추는 코드

---
+++ 에디터 코드/EditModeDetector.cs
@@ -0,0 +1,37 @@
+﻿// 에디터가 실행중인경우 컴파일을 멈추고자 만들어짐
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class EditModeDetectorScript
+{
+    static bool waitingForStop = false;
+
+    static EditModeDetectorScript()
+    {
+        EditorApplication.update += OnEditorUpdate;
+    }
+
+    static void OnEditorUpdate()
+    {
+        if (!waitingForStop
+            && EditorApplication.isCompiling
+            && EditorApplication.isPlaying)
+        {
+            EditorApplication.LockReloadAssemblies();
+            EditorApplication.playModeStateChanged  += PlaymodeChanged;
+            waitingForStop = true;
+        }
+    }
+
+    static void PlaymodeChanged(PlayModeStateChange state)
+    {
+        if (EditorApplication.isPlaying)
+            return;
+
+        EditorApplication.UnlockReloadAssemblies();
+        EditorApplication.playModeStateChanged
+             -= PlaymodeChanged;
+        waitingForStop = false;
+    }
+}
