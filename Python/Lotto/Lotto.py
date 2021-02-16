942821809ca2ac090d299146d82b83b3545dff09
이세종 <sejong@433.co.kr>
2021-02-16 21:21:19 +0900

    Lotto 코드 기본 구상

---
+++ Python/Lotto/Lotto.py
@@ -0,0 +1,23 @@
+"""
+로또 번호 추출기 2.0.0
+Python 버전
+"""
+
+class Lotto( object ):
+    """ 로또 번호 생성기 """
+
+    def __init__(self):
+        self.__InitializeValue();# 초기화 및 번수 선언
+    #__init__
+
+    def __InitializeValue( self ):
+        """ 초기화 및 번수 선언 """
+        pass;
+    #__InitializeValue
+
+    def MakeNumbers( self ):
+        """ 로또 번호를 생성한다. """
+        pass;
+    # MakeNumbers
+
+#Lotto
---
+++ Python/Lotto/Main.py
@@ -0,0 +1,9 @@
+import Lotto as lt;
+
+def run():
+    print( "Hello Python!" );
+    pass;
+
+if __name__ == "__main__":
+    run();
+
