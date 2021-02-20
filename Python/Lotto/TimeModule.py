44166baa72bdd6f0e5f3b4b1017bbcf9bde335f1
이세종 <sejong@433.co.kr>
2021-02-20 22:45:32 +0900

    타임모듈 정리

--- Python/Lotto/TimeModule.py
+++ Python/Lotto/TimeModule.py
@@ -1,3 +1,6 @@
+"""
+시간 관련 모듈 정리
+"""
 import time as TM;
 import datetime as DTM;
 
@@ -8,27 +11,27 @@ def time():
 
 def convert_seconds_to_kor_time( in_seconds ):
     """초를 입력받아 읽기쉬운 한국 시간으로 변환"""
-    t1 = DTM.timedelta( seconds = in_seconds )
-    days = t1.days
-    _sec = t1.seconds
-    (hours, minutes, seconds) = str( DTM.timedelta( seconds = _sec ) ).split( ':' )
-    hours = int( hours )
-    minutes = int( minutes )
-    seconds = int( seconds )
+    t1 = DTM.timedelta( seconds = in_seconds );
+    days = t1.days;
+    total_seconds = t1.seconds;
+    (hours, minutes, seconds) = str( DTM.timedelta( seconds = total_seconds ) ).split( ':' );
+    hours = int( hours );
+    minutes = int( minutes );
+    seconds = int( seconds );
 
-    result = []
+    result = [];
     if days >= 1:
-        result.append( str( days ) + '일' )
+        result.append( str( days ) + "일" );
     if hours >= 1:
-        result.append( str( hours ) + '시간' )
+        result.append( str( hours ) + "시간" );
     if minutes >= 1:
-        result.append( str( minutes ) + '분' )
+        result.append( str( minutes ) + "분" );
     if seconds >= 1:
-        result.append( str( seconds ) + '초' )
-    return ' '.join( result )
+        result.append( str( seconds ) + "초" );
+    return ' '.join( result );
 #convert_seconds_to_kor_time
 
 def convert_seconds_to_time( in_seconds ):
     """초를 입력받아 n days, nn:nn:nn으로 변환"""
-    return str( DTM.timedelta( seconds = in_seconds ) )
+    return str( DTM.timedelta( seconds = in_seconds ) );
 #convert_seconds_to_time
