91cc19a561c3a069fb89603480220eb2842013fd
이세종 <sejong@433.co.kr>
2022-07-23 17:54:14 +0900

    StudyCodeProject Add

---
+++ StudyCodeProject/Assets/Codes/UniTask/UniTaskHow2Use.cs
@@ -0,0 +1,233 @@
+﻿/**---------------------------------------------------------------------------------
+ * @file UniTaskHow2Use.cs
+ * @date 2022/7/13
+ * @author sejong
+ * @brief UniTask 사용법
+ *///-------------------------------------------------------------------------------
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// @class UniTaskHow2Use
+/// @date 2022/7/14
+/// @author sejong
+/// @brief UniTask 사용법
+/// </summary>
+public class UniTaskHow2Use : MonoBehaviour
+{
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	[SerializeField]
+	private Transform _trObj = null;
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+
+	private CancellationToken? _cancellationToken = null;
+
+	private CancellationToken CTS
+	{
+		get 
+		{ 
+			if( _cancellationToken == null )
+			{
+				_cancellationToken = new CancellationToken();
+				_cancellationToken = this.GetCancellationTokenOnDestroy();
+			}
+
+			return _cancellationToken.Value;
+		}
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+
+	/// <summary>
+	/// MonoBehaviour 의 Start 를 사용할 수 있다.
+	/// </summary>
+	/// <returns></returns>
+	protected async UniTaskVoid Start()
+	{
+		var testTkn = UniTaskManager.INSTANCE.GetCancellationToken( "Test" );
+
+		// 일반 함수로 비동기로 처리가 가능하다. 단 이경우 .Forget(); 을 추가해야 워닝이 없다.
+		UniTask.RunOnThreadPool( NormalSyncFunction, cancellationToken: testTkn ).Forget();
+		await UniTask.Delay( 10000 );
+
+		AsynFunction( testTkn ).Forget();
+		await UniTask.Delay( 1000 );
+		UniTaskManager.INSTANCE.CancelTask( "Test" );
+
+		var txt = await UniTaskManager.INSTANCE.WebRequest( "https://www.google.co.jp/" );
+		Debug.Log( txt );
+
+
+		var go = await UniTaskManager.INSTANCE.ResourceLoad<GameObject>( "Cube" );
+
+		/*
+			<color=#0000ff>Check Start!!</color>
+			<color=#ff0000>[WrappingAsync] Start!!</color>
+			[NormalFunction] 0 to 9 : 0
+			<color=#0000ff>Check End!!</color>
+			[NormalFunction] 0 to 9 : 1
+			[NormalFunction] 0 to 9 : 2
+			[NormalFunction] 0 to 9 : 3
+			[NormalFunction] 0 to 9 : 4
+			[NormalFunction] 0 to 9 : 5
+			[NormalFunction] 0 to 9 : 6
+			[NormalFunction] 0 to 9 : 7
+			[NormalFunction] 0 to 9 : 8
+			[NormalFunction] 0 to 9 : 9
+			<color=#ff0000>[WrappingAsync] End!!</color>
+
+			- await 처리를 하지 않은 WrappingAsync 는 비동기로 처리 되지만, 함수 내부는 순서가 보장 된다.
+		*/
+		Debug.Log( $"<color=#0000ff>Check Start!!</color>" );
+		WrappingAsync().Forget();
+		Debug.Log( $"<color=#0000ff>Check End!!</color>" );
+
+		var clone = GameObject.Instantiate( go ) as GameObject;
+		clone.transform.SetParent( _trObj );
+		clone.transform.localPosition	= Vector3.zero;
+		clone.transform.localRotation	= Quaternion.identity;
+		clone.transform.localScale		= Vector3.one;
+
+		await UniTask.Yield();
+
+		await MoveObject( Vector3.zero, Vector3.one * 1000, 10000 );
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 오브젝트 이동
+	/// </summary>
+	/// <param name="from"></param>
+	/// <param name="to"></param>
+	/// <param name="time"></param>
+	/// <returns></returns>
+	private async UniTask<Vector3> MoveObject( Vector3 from, Vector3 to, float time )
+	{
+		if( from == to || time <= 0 )
+			return to;
+
+		float t = 0;
+		
+		while( t < time )
+		{
+			_trObj.localPosition = Vector3.Lerp( from, to, t / time );
+
+			await UniTask.Yield( CTS );
+
+			t += Time.deltaTime;
+		}
+
+		_trObj.localPosition = to;
+
+		return to;
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 비동기 테스트용
+	/// </summary>
+	/// <returns></returns>
+	private async UniTaskVoid WrappingAsync()
+	{
+		Debug.Log( $"<color=#ff0000>[WrappingAsync] Start!!</color>" );
+
+		await NormalAsyncFunction();
+
+		Debug.Log( $"<color=#ff0000>[WrappingAsync] End!!</color>" );
+
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 일반 비동기 처리가 가능한 함수
+	/// </summary>
+	private void NormalSyncFunction()
+	{
+		Debug.Log( $"<color=#ff0000>일반 함수의 비동기 시작!!</color>" );
+		AsynFunction( default).Forget();
+		Debug.Log( $"<color=#ff0000>일반 함수의 비동기 종료!!</color>" );
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 일반 비동기 처리가 가능한 함수
+	/// </summary>
+	private async UniTask NormalAsyncFunction()
+	{
+		for( int i = 0; i < 10; ++i )
+		{
+			Debug.Log( $"[NormalFunction] 0 to 9 : {i}" );
+
+			await UniTask.Delay( 1000 );
+		}
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// UniTaskVoid 리턴형 함수는 외부에서 await 을 통한 대기가 불가능하다.
+	/// </summary>
+	/// <param name="ctn"></param>
+	/// <returns></returns>
+	private async UniTaskVoid AsynFunction( CancellationToken ctn )
+	{
+		for( int i = 0; i < 10; ++i )
+		{
+			Debug.Log( $"0 to 9 : {i}" );
+
+			await UniTask.Delay( 1000, cancellationToken: ctn );
+		}
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 리턴이 있는 경우 처리
+	/// </summary>
+	/// <param name="waitSeconds"></param>
+	/// <returns></returns>
+	private async UniTask<float> WaitSeconds( float waitSeconds )
+	{
+		Debug.Log( $"Start Waiting....." );
+
+		await UniTask.Delay( TimeSpan.FromSeconds( waitSeconds ), cancellationToken: CTS );
+
+		Debug.Log( $"Wait {waitSeconds} Seconds Done!!" );
+
+		return waitSeconds;
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	
+	private void Awake()
+	{
+		
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	
+	private void OnDestroy()
+	{
+		UniTaskManager.Release();
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+}//UniTaskHow2Use
---
+++ StudyCodeProject/Assets/Codes/UniTask/UniTaskManager.cs
@@ -0,0 +1,232 @@
+﻿/**---------------------------------------------------------------------------------
+ * @file UniTaskManager.cs
+ * @date 2022/7/16
+ * @author sejong
+ * @brief UniTask 의 래핑 클래스
+ *///-------------------------------------------------------------------------------
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaseSingleton;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+
+/// <summary>
+/// @class UniTaskManager
+/// @date 2022/7/16
+/// @author sejong
+/// @brief UniTask 의 래핑 클래스
+/// </summary>
+public class UniTaskManager : BaseSingleton<UniTaskManager>, IBaseSingleton
+{
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// CancellationToken 관리용 딕셔너리
+	/// </summary>
+	private Dictionary<string,CancellationTokenSource> _cancelTokenDic = new Dictionary<string, CancellationTokenSource>();
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Unity 통신
+	/// </summary>
+	/// <param name="url"></param>
+	/// <param name="cancleKey"></param>
+	/// <returns></returns>
+	public async UniTask<string> WebRequest( string url, string cancleKey = "" )
+	{
+		// Url 이 비어 있다면 무시한다.
+		if( true == string.IsNullOrEmpty( url ) )
+			return "";
+		
+		try
+		{
+			var cancelToken = GetCancellationToken( cancleKey );
+	
+			var wr = UnityWebRequest.Get( url );
+	
+			if( cancelToken == default )
+				await wr.SendWebRequest();
+			else
+				await wr.SendWebRequest().WithCancellation( cancelToken );
+
+			switch( wr.result )
+			{
+				case UnityWebRequest.Result.InProgress:
+				{
+#if DEBUG
+					Debug.LogError( $"[Error] UnityWebRequest InProgress!!" );
+#endif//DEBUG
+					return "";
+				}
+
+				case UnityWebRequest.Result.ConnectionError:
+				case UnityWebRequest.Result.DataProcessingError:
+				case UnityWebRequest.Result.ProtocolError:
+				{
+#if DEBUG
+					Debug.LogError( $"[Error] UnityWebRequest result = {wr.result}\n - {wr.error}" );
+#endif//DEBUG
+					return "";
+				}
+
+				case UnityWebRequest.Result.Success:
+					break;
+			}
+
+			return wr.downloadHandler.text;
+		}
+		catch( System.Exception ex )
+		{
+#if DEBUG
+			Debug.Log( $"[Exception] UnityWebRequest = {ex.Message}" );
+#endif//DEBUG
+			return "";
+		}
+
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 리소스 로딩을 비동기로 처리한다.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="path"></param>
+	/// <param name="cancleKey"></param>
+	/// <returns></returns>
+	public async UniTask<T> ResourceLoad<T>( string path, string cancleKey = "" ) where T : UnityEngine.Object
+	{
+		// path 가 비어 있다면 무시한다.
+		if( true == string.IsNullOrEmpty( path ) )
+			return null;
+
+		try
+		{
+			var res = default( UnityEngine.Object );
+			var cancelToken = GetCancellationToken( cancleKey );
+
+			if( cancelToken == default )
+				res = await Resources.LoadAsync<T>( path );
+			else
+				res = await Resources.LoadAsync<T>( path ).WithCancellation( cancelToken );
+
+			return res as T;
+		}
+		catch( Exception ex )
+		{
+#if DEBUG
+			Debug.Log( $"[Exception] Resources.LoadAsync = {ex.Message}" );
+#endif//DEBUG
+			return null;
+		}
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	
+	/// <summary>
+	/// 여러개의 캔슬 토큰을 묶어서 하나로 만들어 준다.
+	/// </summary>
+	/// <param name="ctns"></param>
+	/// <returns></returns>
+	public CancellationTokenSource LinkCancellationToken( params CancellationToken[] ctns )
+	{
+		var linkedCtn = CancellationTokenSource.CreateLinkedTokenSource( ctns );
+
+		return linkedCtn; 
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 캔슬 토큰을 얻어 온다.\n
+	/// 캔슬 토큰은 비동기 작업 함수 내에서 리스너 역할을 한다.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <returns></returns>
+	public CancellationToken GetCancellationToken( string key )
+	{
+		if( true == string.IsNullOrEmpty( key ) )
+			return default;
+
+		// 이미 있는 경우에
+		if( true == _cancelTokenDic.ContainsKey( key ) )
+		{
+			// 사용한 적이 없다면 그대로 리턴
+			if( false == _cancelTokenDic[key].IsCancellationRequested )
+				return _cancelTokenDic[ key ].Token;
+
+			// 사용한 적이 있으면 제거
+			_cancelTokenDic[ key ].Dispose();
+			_cancelTokenDic.Remove( key );
+		}
+
+		// 할당 한다.
+		_cancelTokenDic[ key ] = new CancellationTokenSource();
+
+		return _cancelTokenDic[ key ].Token;
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 해당 비동기 작업을 캔슬 한다.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="isForce">예외를 즉시 전파해야 하는 경우 true이고, 그렇지 않으면 false입니다</param>
+	public void CancelTask( string key, bool isForce = false )
+	{
+		if( false == _cancelTokenDic.ContainsKey( key ) )
+		{
+#if DEBUG
+			Debug.Log( $"[UniTaskManager] {key} 는 등록되지 않은 캔슬 토큰 입니다." );
+#endif//DEBUG
+			return;
+		}
+
+		/// 중지 시킨 후에는 지워준다.
+		_cancelTokenDic[ key ].Cancel( isForce );
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	
+	#region IBaseSingleton
+
+	/// <summary>
+	/// 초기화
+	/// </summary>
+	public void Initialize()
+	{
+		Dispose();
+				
+	}
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	
+	/// <summary>
+	/// 릴리즈 함수 
+	/// </summary>
+	public void Dispose()
+	{
+		// 살아 있는 토큰은 모두 정리하고 클리어
+		foreach( var ctn in _cancelTokenDic.Values )
+		{
+			ctn.Cancel();
+			ctn.Dispose();
+		}
+
+		_cancelTokenDic.Clear();
+
+	}
+
+	#endregion//IBaseSingleton
+
+	//@@-------------------------------------------------------------------------------------------------------------------------
+	//@@-------------------------------------------------------------------------------------------------------------------------
+
+}//UniTaskManager
