bbeacfa37dc3d952faf514b5acbd0123937ddeee
이세종 <sejong@433.co.kr>
2022-10-24 20:24:49 +0900

    uGUI 공부

---
+++ StudyCodeProject/Assets/Codes/UGUI/UGUI_How2Use.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UGUI_How2Use : MonoBehaviour
+{
+	public void OnClick_Start()
+	{
+		Debug.LogFormat( "[UGUI_How2Use.OnClick_Start] Clicked" );
+	}
+
+	public void OnClick_Quit()
+	{
+
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else//UNITY_EDITOR
+		Application.Quit();
+#endif//UNITY_EDITOR
+	
+	}
+
+}//UGUI_How2Use
